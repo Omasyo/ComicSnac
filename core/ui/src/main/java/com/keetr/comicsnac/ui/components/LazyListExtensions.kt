b838f604a7d2c4cc1eb36795613c4304dad4b3db
omasyo <cesare3000@outlook.com>
2023-08-13 08:58:28 +0100

    made lazylist wrapper

--- core/ui/src/main/java/com/keetr/comicsnac/ui/components/LazyListExtensions.kt
+++ core/ui/src/main/java/com/keetr/comicsnac/ui/components/LazyListExtensions.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,38 +17,56 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.keetr.comicsnac.ui.theme.ComicSnacTheme
 
-//class ListScope {
-//
-//}
-//
-//fun LazyListScope.panelList(content: ())
+data class PanelColors(
+    val strokeColor: Color,
+    val surface1: Color,
+    val surface2: Color,
+    val surface3: Color,
+)
 
-fun LazyListScope.panelSeperator(
-    modifier: Modifier = Modifier,
-    upperColor: Color,
-    lowerColor: Color,
-    strokeColor: Color,
-    flipped: Boolean,
-) {
-    item {
-        ComicListSeperator(
-            upperColor = upperColor,
-            lowerColor = lowerColor,
-            strokeColor = strokeColor,
-            flipped = flipped
-        )
+class ListScope(colors: PanelColors) {
+
+    private val colors: List<Color> = listOf(colors.surface1, colors.surface2, colors.surface3)
+    private val strokeColor: Color = colors.strokeColor
+
+    private var currColorIndex = 0
+    private val nextColorIndex get() = (currColorIndex + 1) % 3
+
+    private var seperatorCount = 0
+
+    fun LazyListScope.panelSeperator(
+        modifier: Modifier = Modifier,
+    ) {
+        item("separator#${++seperatorCount}") {
+            ComicListSeperator(
+                upperColor = colors[currColorIndex],
+                lowerColor = colors[nextColorIndex],
+                strokeColor = strokeColor,
+                flipped = currColorIndex % 2 != 0
+            )
+            currColorIndex = nextColorIndex
+        }
+    }
+
+    fun LazyListScope.panel(
+        tag: String,
+        backgroundColor: Color,
+        content: @Composable BoxScope.() -> Unit
+    ) {
+        item(tag) {
+            Box(Modifier.background(colors[currColorIndex]), content = content)
+        }
     }
 }
 
-fun LazyListScope.panel(
-    tag: String,
-    backgroundColor: Color,
-    content: @Composable BoxScope.() -> Unit
+fun LazyListScope.panelList(
+    colors: PanelColors,
+    content: ListScope.() -> Unit,
 ) {
-    item(tag) {
-        Box(Modifier.background(backgroundColor), content = content)
-    }
+    val listScope = ListScope(colors)
+    listScope.content()
 }
+
 
 @Preview
 @Composable
@@ -59,66 +78,55 @@ private fun Preview() {
         val strokeColor = MaterialTheme.colorScheme.onSurface
 
         LazyColumn {
+            panelList(
+                PanelColors(strokeColor, primary, secondary, tetiary)
+            ) {
 
-            panel("Test 1", primary) {
-                Box(
-                    Modifier
-                        .padding(24f.dp)
-                        .background(Color.Cyan)
-                        .fillMaxWidth()
-                        .height(80f.dp)
-                )
-            }
+                panel("Test 1", primary) {
+                    Box(
+                        Modifier
+                            .padding(24f.dp)
+                            .background(Color.Cyan)
+                            .fillMaxWidth()
+                            .height(80f.dp)
+                    )
+                }
 
-            panelSeperator(
-                upperColor = primary,
-                lowerColor = secondary,
-                strokeColor = strokeColor,
-                flipped = false
-            )
+                panelSeperator()
 
-            panel("Test 2", secondary) {
-                Box(
-                    Modifier
-                        .padding(24f.dp)
-                        .background(Color.Magenta)
-                        .fillMaxWidth()
-                        .height(80f.dp)
-                )
-            }
+                panel("Test 2", secondary) {
+                    Box(
+                        Modifier
+                            .padding(24f.dp)
+                            .background(Color.Magenta)
+                            .fillMaxWidth()
+                            .height(80f.dp)
+                    )
+                }
 
-            panelSeperator(
-                upperColor = secondary,
-                lowerColor = tetiary,
-                strokeColor = strokeColor,
-                flipped = true
-            )
+                panelSeperator()
 
-            panel("Test 3", tetiary) {
-                Box(
-                    Modifier
-                        .padding(24f.dp)
-                        .background(Color.DarkGray)
-                        .fillMaxWidth()
-                        .height(80f.dp)
-                )
-            }
+                panel("Test 3", tetiary) {
+                    Box(
+                        Modifier
+                            .padding(24f.dp)
+                            .background(Color.DarkGray)
+                            .fillMaxWidth()
+                            .height(80f.dp)
+                    )
+                }
 
-            panelSeperator(
-                upperColor = tetiary,
-                lowerColor = primary,
-                strokeColor = strokeColor,
-                flipped = false
-            )
+                panelSeperator()
 
-            panel("Test 4", primary) {
-                Box(
-                    Modifier
-                        .padding(24f.dp)
-                        .background(Color.DarkGray)
-                        .fillMaxWidth()
-                        .height(80f.dp)
-                )
+                panel("Test 4", primary) {
+                    Box(
+                        Modifier
+                            .padding(24f.dp)
+                            .background(Color.DarkGray)
+                            .fillMaxWidth()
+                            .height(80f.dp)
+                    )
+                }
             }
         }
     }
